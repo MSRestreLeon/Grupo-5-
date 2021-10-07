b4f5bd9f854f5c71d6ccacf057287a4fa88e4cbd
David Alejandro Mayama Quintero <dmayama@uninorte.edu.co>
2021-10-06 22:18:27 -0500

    Se agregan rutas de super administrador

--- 003_proyecto/main.py
+++ 003_proyecto/main.py
@@ -7,28 +7,44 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
-
-@app.route("/paciente")
-def paciente():
-    return render_template("paciente.html")
-
-
+# Rutas medico
 @app.route("/medico")
 def medico():
     return render_template("medico.html")
 
-@app.route("/historia_clinica")
+@app.route("/medico/historiaClinica")
 def historia_clinica():
-    return render_template("historia_clinica.html")
+    return render_template("historiaClinica.html")
 
-@app.route("/consulta_medica")
+@app.route("/medico/consultaMedica")
 def consulta_medica():
-    return render_template("consulta_medica.html")
+    return render_template("consultaMedica.html")
 
-
+# Rutas Super Administrador   
 @app.route("/superAdministrador")
 def superAdministrador():
     return render_template("superAdministrador.html")
+
+@app.route("/superAdministrador/agendaMedica")
+def superAdministradorAgendaMedica():
+    return render_template("agendaSuper.html")
+
+@app.route("/superAdministrador/dashboard")
+def superAdministradorDashboard():
+    return render_template("dashboard.html")
+
+@app.route("/superAdministrador/historiaClinica")
+def superAdministradorHistoriaClinica():
+    return render_template("historiaClinicaSuper.html")
+
+@app.route("/superAdministrador/aperturaAgenda")
+def superAdministradorAperturaAgenda():
+    return render_template("aperturaAgenda.html")
+
+#Rutas Paciente
+@app.route("/paciente")
+def paciente():
+    return render_template("paciente.html")
 
 app.run(debug=True)
 
