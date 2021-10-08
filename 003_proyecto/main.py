71b759ac779f8c7000302cd2b1e55602b76020f6
luifrancgom <luifrancgom@gmail.com>
2021-10-07 20:42:22 -0500

    Adición de secciones paciente, manejo y recuperación de contraseña y registro

--- 003_proyecto/main.py
+++ 003_proyecto/main.py
@@ -2,49 +2,88 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Login
+
 
 @app.route("/")
 def login():
     return render_template("login.html")
 
+
+@app.route("/registrarse")
+def registrarse():
+    return render_template("registrarse.html")
+
+
+@app.route("/recuperarContrasena")
+def recuperarContrasena():
+    return render_template("recuperarContrasena.html")
+
+
+@app.route("/generarNuevaContrasena")
+def generarNuevaContrasena():
+    return render_template("generarNuevaContrasena.html")
+
 # Rutas medico
+
+
 @app.route("/medico")
 def medico():
     return render_template("medico.html")
+
 
 @app.route("/medico/historiaClinica")
 def historia_clinica():
     return render_template("historiaClinica.html")
 
+
 @app.route("/medico/consultaMedica")
 def consulta_medica():
     return render_template("consultaMedica.html")
 
-# Rutas Super Administrador   
+# Rutas Super Administrador
+
+
 @app.route("/superAdministrador")
 def superAdministrador():
     return render_template("superAdministrador.html")
+
 
 @app.route("/superAdministrador/agendaMedica")
 def superAdministradorAgendaMedica():
     return render_template("agendaSuper.html")
 
+
 @app.route("/superAdministrador/dashboard")
 def superAdministradorDashboard():
     return render_template("dashboard.html")
+
 
 @app.route("/superAdministrador/historiaClinica")
 def superAdministradorHistoriaClinica():
     return render_template("historiaClinicaSuper.html")
 
+
 @app.route("/superAdministrador/aperturaAgenda")
 def superAdministradorAperturaAgenda():
     return render_template("aperturaAgenda.html")
 
-#Rutas Paciente
+# Rutas Paciente
+
+
 @app.route("/paciente")
 def paciente():
     return render_template("paciente.html")
 
-app.run(debug=True)
 
+@app.route("/paciente/citaMedica")
+def paciente():
+    return render_template("citaMedica.html")
+
+
+@app.route("/paciente/examenMedico")
+def paciente():
+    return render_template("examenMedico.html")
+
+
+app.run(debug=True)
